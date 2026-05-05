cbf43d2917c26b35703a1abd6b43c52dbccad669
Darko Bozic <darko.zmaj.bozic@gmail.com>
2026-05-06 01:18:24 +0200

    Controler+query

---
+++ SmartApiary.Application/Features/Crops/Queries/GetCropsQuery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartApiary.Application.Features.Crops.Queries;
+
+using MediatR;
+using SmartApiary.Domain.Models;
+using SmartApiary.Application.Common.Interfaces;
+
+public record GetCropsQuery(Guid ParcelId) : IRequest<IReadOnlyCollection<Crop>>;
+
+public class GetCropsQueryHandler(
+    ICropRepository cropRepository
+) : IRequestHandler<GetCropsQuery, IReadOnlyCollection<Crop>>
+{
+    public async Task<IReadOnlyCollection<Crop>> Handle(GetCropsQuery request, CancellationToken ct)
+    {
+        return await cropRepository.GetByParcelIdAsync(request.ParcelId, ct);
+    }
+}
---
+++ SmartApiary.WebApi/Controllers/FarmerCropsController.cs
@@ -0,0 +1,27 @@
+﻿namespace SmartApiary.WebApi.Controllers;
+
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SmartApiary.Application.Features.Crops.Commands;
+using SmartApiary.Application.Features.Crops.Queries;
+
+[ApiController]
+[Route("api/farmer/crops")]
+[Authorize(Roles = "Farmer")]
+public class FarmerCropsController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateCropCommand command, CancellationToken ct)
+    {
+        var id = await mediator.Send(command, ct);
+        return Ok(id);
+    }
+
+    [HttpGet("{parcelId}")]
+    public async Task<IActionResult> Get(Guid parcelId, CancellationToken ct)
+    {
+        var crops = await mediator.Send(new GetCropsQuery(parcelId), ct);
+        return Ok(crops);
+    }
+}
